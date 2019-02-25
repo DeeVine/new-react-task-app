c92cf6022d70208a9bb7d737d848003639df73fe
DeeVine <kwongdarren@gmail.com>
2019-02-24 18:59:18 -0800

    added in set interval timer

--- src/components/taskapp/timer/timer.js
+++ src/components/taskapp/timer/timer.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import moment from 'moment'
 import { Container, Row, Col, Button } from 'reactstrap'
 
 //TODO:
+
+//start time, end time
+  //timer for clock utilizing moment add 'moment().add(1, 'second')'
+    //calculate difference from start time and current time in seconds
+      //rerender clock
+
 //save timer state to local localStorage
 //measure time with set interval or calculate difference in start and end times
   //simulate timer
@@ -13,11 +20,11 @@ import { Container, Row, Col, Button } from 'reactstrap'
 export default class Timer extends React.Component {
   constructor(props){
     super(props)
-
     this.state = {
       workingOnInput: '',
-      timeStarted: true,
-      timer: ''
+      timeStarted: false,
+      currentTime: moment().startOf("day"),
+      timer: '',
     }
 }
 
@@ -26,6 +33,21 @@ export default class Timer extends React.Component {
     this.setState({
       workingOnInput: text
     })
+  }
+
+  startTime = () => {
+    this.toggleTimer()
+    const setTimer = () => {
+      this.setState(prevState => ({
+        currentTime: prevState.currentTime.add(1, 'second'),
+      }))
+    }
+    this.interval = window.setInterval(() => {setTimer()}, 1000)
+  }
+
+  stopTime = () => {
+    this.toggleTimer()
+    window.clearInterval(this.interval);
   }
 
   toggleTimer = () => {
@@ -40,12 +62,11 @@ export default class Timer extends React.Component {
         <Row>
           <Col className='timer-nav' sm={12}>
             <input onChange={this.updateInput} value={this.state.workingOnInput} placeholder={'What are you working on?'}/>
-            {this.state.timeStarted
-              ? <Button onClick={this.toggleTimer} color="primary">Start</Button>
-              : <Button onClick={this.toggleTimer} color="success">Finish</Button>
+            <span>{this.state.currentTime.format('HH:mm:ss')}</span>
+            {!this.state.timeStarted
+              ? <Button onClick={this.startTime} color="success">Start</Button>
+              : <Button onClick={this.stopTime} color="danger">Stop</Button>
             }
-            {/* <Button onClick={this.toggleTimer} color="primary">Start</Button>
-            <Button onClick={this.toggleTimer} color="success">Finish</Button> */}
           </Col>
           <Col className='timer-time'>
             Timer Section

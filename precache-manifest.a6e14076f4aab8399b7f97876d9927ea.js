f9c7114b2d85768d790797ada41340ed5c381ba9
darren <kwongdarren@gmail.com>
2019-03-27 00:59:43 -0700

    Updates

---
+++ precache-manifest.a6e14076f4aab8399b7f97876d9927ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84bc8f01c960d9218114",
+    "url": "/new-react-task-app/static/css/main.bf7ea9b0.chunk.css"
+  },
+  {
+    "revision": "84bc8f01c960d9218114",
+    "url": "/new-react-task-app/static/js/main.84bc8f01.chunk.js"
+  },
+  {
+    "revision": "262ce1467307923846f2",
+    "url": "/new-react-task-app/static/css/1.48f34b31.chunk.css"
+  },
+  {
+    "revision": "262ce1467307923846f2",
+    "url": "/new-react-task-app/static/js/1.262ce146.chunk.js"
+  },
+  {
+    "revision": "c336b7cd9e90190830ae",
+    "url": "/new-react-task-app/static/js/runtime~main.c336b7cd.js"
+  },
+  {
+    "revision": "4b27b1af94520acbf434996fc2b195c8",
+    "url": "/new-react-task-app/index.html"
+  }
+];
